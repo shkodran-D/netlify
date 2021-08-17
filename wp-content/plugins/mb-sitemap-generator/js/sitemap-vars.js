dcb030fb163f47756a99aca0c0b042afc18145ce
shkodran-D <islamishkodran@gmail.com>
2021-08-17 22:22:53 +0200

    d

--- wp-content/plugins/mb-sitemap-generator/js/sitemap-vars.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var sitemapGeneratorVars = {
-	systemName: 		'WordPress',
-	proxyURL: 			'admin-ajax.php?action=sitemap_proxy',
-	professionalURL: 	'https://www.marcobeierer.com/wordpress-plugins/sitemap-generator-professional',
-	btnPrimaryClass: 	'button-primary',
-	btnDefaultClass: 	'button-default',
-}
